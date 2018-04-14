b4b2f97a66a86148f7e9d2409c3ea44293920336
Ashton <ashchristie@gmail.com>
2018-04-14 13:34:08 -0700

    added array chip stack

--- src/index.js
+++ src/index.js
@@ -5,22 +5,35 @@ const button_double = document.querySelector('.btn--double');
 const button_split = document.querySelector('.btn--split');
 
 // Chips
-const button_chipBet = document.querySelectorAll('.chip--bet');
-const button_chipPlayer = document.querySelector('.chip--player');
+const button_chipPlayer = document.querySelectorAll('.chip--player');
+const element_chipPot = document.querySelector('.chip--pot');
 
 // Score
+const element_chipScore = document.querySelector('.chip--score');
 const element_playerScore = document.querySelector('.player__score');
 
-let playerScore = 2400;
-let playerBet;
+let playerScore = 2500;
+let playerBet = [];
 
-const updateScore = function () {
+const updateBet = function () {
+  const playerBetTotal = playerBet.reduce((total, current) => {
+    return total + current;
+  }, 0);
+  console.log(playerBetTotal);
   element_playerScore.innerText = playerScore;
+  element_chipScore.innerText = playerBetTotal;
 }
 
 
-const chipBet = function (chipValue) {
- playerBet += chipValue
+const chipBetAdd = function () {
+  betSize = parseInt(this.dataset.bet);
+  // console.log(betSize);
+
+  playerScore -= betSize;
+  playerBet.push(betSize);
+
+
+  updateBet();
 }
 
 
@@ -38,10 +51,8 @@ button_stand.addEventListener('click', alert);
 button_double.addEventListener('click', alert);
 button_split.addEventListener('click', alert);
 
-button_chipBet.forEach(chip => {
-  addEventListener('click', alert);
-});
-button_chipPlayer.addEventListener('click', alert);
+button_chipPlayer.forEach(chip => chip.addEventListener('click', chipBetAdd));
 
+element_chipPot.addEventListener('click', alert);
 
-updateScore();
+updateBet();
