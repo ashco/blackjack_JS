55e2a182bc8704b138061735489991fa269e4403
Ashton <ashchristie@gmail.com>
2018-04-15 21:37:13 -0700

    redid chip bet logic to work with array

--- src/index.js
+++ src/index.js
@@ -1,58 +1,68 @@
 // Buttons
+const button_play = document.querySelector('.btn--play');
 const button_hit = document.querySelector('.btn--hit');
 const button_stand = document.querySelector('.btn--stand');
 const button_double = document.querySelector('.btn--double');
 const button_split = document.querySelector('.btn--split');
 
 // Chips
-const button_chipPlayer = document.querySelectorAll('.chip--player');
-const element_chipPot = document.querySelector('.chip--pot');
+const element_chipPlayer = document.querySelectorAll('.chip--player');
+const element_chipPot = document.querySelector('.chip__pot');
 
 // Score
-const element_chipScore = document.querySelector('.chip--score');
+const element_chipScore = document.querySelector('.chip__score');
 const element_playerScore = document.querySelector('.player__score');
 
 let playerScore = 2500;
-let playerBet = [];
+let playerBetArr = [];
 
-const updateBet = function () {
-  const playerBetTotal = playerBet.reduce((total, current) => {
-    return total + current;
-  }, 0);
-  console.log(playerBetTotal);
+
+const bet_chipAdd = function () {
+  let chipSize = parseInt(this.dataset.bet);
+
+  playerBetArr.push(chipSize);
+  playerScore = playerScore - chipSize;
+
+  bet_update();
+  chipDisableCheck();
+}
+
+const bet_chipRemove = function () {
+  const chipSize = playerBetArr.pop();
+
+  playerScore = playerScore + chipSize;
+
+  bet_update();
+  chipDisableCheck();
+}
+
+const bet_update = function () {
+  const playerBetTotal = playerBetArr.reduce((total, current) => total + current, 0);
+
+
   element_playerScore.innerText = playerScore;
   element_chipScore.innerText = playerBetTotal;
 }
 
+const chipDisableCheck = function () {
+  element_chipPlayer.forEach(chip => {
+    let chipSize = parseInt(chip.dataset.bet);
 
-const chipBetAdd = function () {
-  betSize = parseInt(this.dataset.bet);
-  // console.log(betSize);
-
-  playerScore -= betSize;
-  playerBet.push(betSize);
-
-
-  updateBet();
+    chipSize > playerScore ? chip.classList.add('disabled') : chip.classList.remove('disabled');
+  });
 }
 
 
+const alert = () => console.log('trigger');
 
 
-
-
-const alert = function () {
-  console.log('trigger');
-}
-
-
+button_play.addEventListener('click', alert);
 button_hit.addEventListener('click', alert);
 button_stand.addEventListener('click', alert);
 button_double.addEventListener('click', alert);
 button_split.addEventListener('click', alert);
 
-button_chipPlayer.forEach(chip => chip.addEventListener('click', chipBetAdd));
+element_chipPot.addEventListener('click', bet_chipRemove);
+element_chipPlayer.forEach(chip => chip.addEventListener('click', bet_chipAdd));
 
-element_chipPot.addEventListener('click', alert);
-
-updateBet();
+bet_update();
